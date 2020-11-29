024727b35a3e6ad02c79eae07e707e23155be593
hsyliark <sungyoonhwang3@gmail.com>
2020-11-29 11:03:16 +0900

    add

--- 논문데이터분석(GAM,VCmodel)/연습용csv파일/try5/10-fold crossvalidation.R
+++ 논문데이터분석(GAM,VCmodel)/연습용csv파일/try5/10-fold crossvalidation.R
@@ -67,7 +67,8 @@ TC1.RMSE <- data.frame(RMSE=c(TC1.RMSE.mlr,TC1.RMSE.glm,
                               TC1.RMSE.gam,TC1.RMSE.tvcm),
                        model=c(rep("1_MLR",10),rep("2_GLM",10),
                                rep("3_GAM",10),rep("4_TVCM",10)))
-ggplot(TC1.RMSE, aes(x=model, y=RMSE, fill=model)) + geom_boxplot()
+ggplot(TC1.RMSE, aes(x=model, y=RMSE, fill=model)) + geom_boxplot() + 
+  ggtitle("Gwangsan TC")
 
 
 # 광산 FC
@@ -137,7 +138,8 @@ FC1.RMSE <- data.frame(RMSE=c(FC1.RMSE.mlr,FC1.RMSE.glm,
                               FC1.RMSE.gam,FC1.RMSE.tvcm),
                        model=c(rep("1_MLR",10),rep("2_GLM",10),
                                rep("3_GAM",10),rep("4_TVCM",10)))
-ggplot(FC1.RMSE, aes(x=model, y=RMSE, fill=model)) + geom_boxplot()
+ggplot(FC1.RMSE, aes(x=model, y=RMSE, fill=model)) + geom_boxplot() + 
+  ggtitle("Gwangsan FC")
 
 
 # 광산 Chla
@@ -207,6 +209,231 @@ Chla1.RMSE <- data.frame(RMSE=c(Chla1.RMSE.mlr,Chla1.RMSE.glm,
                                 Chla1.RMSE.gam,Chla1.RMSE.tvcm),
                        model=c(rep("1_MLR",10),rep("2_GLM",10),
                                rep("3_GAM",10),rep("4_TVCM",10)))
-ggplot(Chla1.RMSE, aes(x=model, y=RMSE, fill=model)) + geom_boxplot()
+ggplot(Chla1.RMSE, aes(x=model, y=RMSE, fill=model)) + geom_boxplot() + 
+  ggtitle("Gwangsan Chla")
   
   
+
+
+
+
+
+
+
+
+
+
+
+
+## 우치 TC
+
+TC2.RMSE.mlr <- c()
+TC2.RMSE.glm <- c()
+TC2.RMSE.gam <- c()
+TC2.RMSE.tvcm <- c()
+for (i in 1:10) {
+  a <- sample(1:nrow(ex2),round(4*nrow(ex2)/10))
+  train <- ex2[-a,] ; test <- ex2[a,]
+  # Multiple Linear Regression
+  fit <- glm(TC~pH+DO+BOD+COD+SS+TN+TP+TOC+WT+EC+log(Chla)
+             +NH3N+PO4P+log(FC)+Flow+Rain,data=train,
+             family=gaussian(link="identity"))
+  fit.step <- stepAIC(fit, direction="both", trace=FALSE)
+  pred.mlr <- predict(fit.step,newdata=test,type="response")
+  data.mlr <- data.frame(response=test$TC,fitted_values=pred.mlr,
+                         time=test$time)
+  TC2.RMSE.mlr <- c(TC2.RMSE.mlr,
+                    sqrt(sum((data.mlr$response-data.mlr$fitted_values)^2)/
+                           length(data.mlr$response)))
+  print(c('MLR',i))
+  # Generalized Linear Model
+  m <- glm(TC~pH+DO+BOD+COD+SS+TN+TP+TOC+WT+EC+log(Chla)+
+             NH3N+PO4P+log(FC)+Flow+Rain,data=train,
+           family=poisson(link="log"))
+  m.step <- stepAIC(m, direction="both", trace=FALSE)
+  pred.glm <- predict(m.step,newdata=test,type="response")
+  data.glm <- data.frame(response=test$TC,fitted_values=pred.glm,
+                         time=test$time)
+  TC2.RMSE.glm <- c(TC2.RMSE.glm,
+                    sqrt(sum((data.glm$response-data.glm$fitted_values)^2)/
+                           length(data.glm$response)))
+  print(c('GLM',i))
+  # Generalized Additive Model
+  mm.shrink <- gam(TC~s(pH)+s(DO)+s(BOD)+s(COD)+s(SS)+s(TN)+s(TP)
+                   +s(TOC)+s(WT)+s(EC)+s(log(Chla))+s(NH3N)+s(PO4P)
+                   +s(log(FC))+s(Flow)+s(Rain),data=train,
+                   family=quasipoisson(link="log"),method="GCV.Cp",
+                   select=TRUE)
+  pred.gam <- predict(mm.shrink,newdata=test,type="response")
+  data.gam <- data.frame(response=test$TC,fitted_values=pred.gam,
+                         time=test$time)
+  TC2.RMSE.gam <- c(TC2.RMSE.gam,
+                    sqrt(sum((data.gam$response-data.gam$fitted_values)^2)/
+                           length(data.gam$response)))
+  print(c('GAM',i))
+  # Time Varying Coefficient Model
+  vc.shrink <- gam(TC~s(time)+s(time,by=pH)+s(time,by=DO)+
+                     s(time,by=BOD)+s(time,by=COD)+s(time,by=SS)+
+                     s(time,by=TN)+s(time,by=TP)+s(time,by=TOC)+
+                     s(time,by=WT)+s(time,by=EC)+s(time,by=log(Chla))+
+                     s(time,by=NH3N)+s(time,by=PO4P)+s(time,by=log(FC))+
+                     s(time,by=Flow)+s(time,by=Rain),data=train,
+                   family=quasipoisson(link="log"),method="GCV.Cp",
+                   select=TRUE)
+  pred.tvcm <- predict(vc.shrink,newdata=test,type="response")
+  data.tvcm <- data.frame(response=test$TC,fitted_values=pred.tvcm,
+                          time=test$time)
+  TC2.RMSE.tvcm <- c(TC2.RMSE.tvcm,
+                     sqrt(sum((data.tvcm$response-data.tvcm$fitted_values)^2)/
+                            length(data.tvcm$response)))
+  print(c('TVCM',i))
+}
+TC2.RMSE <- data.frame(RMSE=c(TC2.RMSE.mlr,TC2.RMSE.glm,
+                              TC2.RMSE.gam,TC2.RMSE.tvcm),
+                       model=c(rep("1_MLR",10),rep("2_GLM",10),
+                               rep("3_GAM",10),rep("4_TVCM",10)))
+ggplot(TC2.RMSE, aes(x=model, y=RMSE, fill=model)) + geom_boxplot() + 
+  ggtitle("Uchi TC")
+
+
+# 우치 FC
+
+FC2.RMSE.mlr <- c()
+FC2.RMSE.glm <- c()
+FC2.RMSE.gam <- c()
+FC2.RMSE.tvcm <- c()
+for (i in 1:10) {
+  a <- sample(1:nrow(ex2),round(4*nrow(ex2)/10))
+  train <- ex2[-a,] ; test <- ex2[a,]
+  # Multiple Linear Regression
+  fit <- glm(FC~pH+DO+BOD+COD+SS+TN+TP+TOC+WT+EC+log(Chla)
+             +NH3N+PO4P+log(TC)+Flow+Rain,data=train,
+             family=gaussian(link="identity"))
+  fit.step <- stepAIC(fit, direction="both", trace=FALSE)
+  pred.mlr <- predict(fit.step,newdata=test,type="response")
+  data.mlr <- data.frame(response=test$FC,fitted_values=pred.mlr,
+                         time=test$time)
+  FC2.RMSE.mlr <- c(FC2.RMSE.mlr,
+                    sqrt(sum((data.mlr$response-data.mlr$fitted_values)^2)/
+                           length(data.mlr$response)))
+  print(c('MLR',i))
+  # Generalized Linear Model
+  m <- glm(FC~pH+DO+BOD+COD+SS+TN+TP+TOC+WT+EC+log(Chla)+
+             NH3N+PO4P+log(TC)+Flow+Rain,data=train,
+           family=poisson(link="log"))
+  m.step <- stepAIC(m, direction="both", trace=FALSE)
+  pred.glm <- predict(m.step,newdata=test,type="response")
+  data.glm <- data.frame(response=test$FC,fitted_values=pred.glm,
+                         time=test$time)
+  FC2.RMSE.glm <- c(FC2.RMSE.glm,
+                    sqrt(sum((data.glm$response-data.glm$fitted_values)^2)/
+                           length(data.glm$response)))
+  print(c('GLM',i))
+  # Generalized Additive Model
+  mm.shrink <- gam(FC~s(pH)+s(DO)+s(BOD)+s(COD)+s(SS)+s(TN)+s(TP)
+                   +s(TOC)+s(WT)+s(EC)+s(log(Chla))+s(NH3N)+s(PO4P)
+                   +s(log(TC))+s(Flow)+s(Rain),data=train,
+                   family=quasipoisson(link="log"),method="GCV.Cp",
+                   select=TRUE)
+  pred.gam <- predict(mm.shrink,newdata=test,type="response")
+  data.gam <- data.frame(response=test$FC,fitted_values=pred.gam,
+                         time=test$time)
+  FC2.RMSE.gam <- c(FC2.RMSE.gam,
+                    sqrt(sum((data.gam$response-data.gam$fitted_values)^2)/
+                           length(data.gam$response)))
+  print(c('GAM',i))
+  # Time Varying Coefficient Model
+  vc.shrink <- gam(FC~s(time)+s(time,by=pH)+s(time,by=DO)+
+                     s(time,by=BOD)+s(time,by=COD)+s(time,by=SS)+
+                     s(time,by=TN)+s(time,by=TP)+s(time,by=TOC)+
+                     s(time,by=WT)+s(time,by=EC)+s(time,by=log(Chla))+
+                     s(time,by=NH3N)+s(time,by=PO4P)+s(time,by=log(TC))+
+                     s(time,by=Flow)+s(time,by=Rain),data=train,
+                   family=quasipoisson(link="log"),method="GCV.Cp",
+                   select=TRUE)
+  pred.tvcm <- predict(vc.shrink,newdata=test,type="response")
+  data.tvcm <- data.frame(response=test$FC,fitted_values=pred.tvcm,
+                          time=test$time)
+  FC2.RMSE.tvcm <- c(FC2.RMSE.tvcm,
+                     sqrt(sum((data.tvcm$response-data.tvcm$fitted_values)^2)/
+                            length(data.tvcm$response)))
+  print(c('TVCM',i))
+}
+FC2.RMSE <- data.frame(RMSE=c(FC2.RMSE.mlr,FC2.RMSE.glm,
+                              FC2.RMSE.gam,FC2.RMSE.tvcm),
+                       model=c(rep("1_MLR",10),rep("2_GLM",10),
+                               rep("3_GAM",10),rep("4_TVCM",10)))
+ggplot(FC2.RMSE, aes(x=model, y=RMSE, fill=model)) + geom_boxplot() + 
+  ggtitle("Uchi FC")
+
+
+# 광산 Chla
+
+Chla2.RMSE.mlr <- c()
+Chla2.RMSE.glm <- c()
+Chla2.RMSE.gam <- c()
+Chla2.RMSE.tvcm <- c()
+for (i in 1:10) {
+  a <- sample(1:nrow(ex2),round(4*nrow(ex2)/10))
+  train <- ex2[-a,] ; test <- ex2[a,]
+  # Multiple Linear Regression
+  fit <- glm(Chla~pH+DO+BOD+COD+SS+TN+TP+TOC+WT+EC+log(FC)
+             +NH3N+PO4P+log(TC)+Flow+Rain,data=train,
+             family=gaussian(link="identity"))
+  fit.step <- stepAIC(fit, direction="both", trace=FALSE)
+  pred.mlr <- predict(fit.step,newdata=test,type="response")
+  data.mlr <- data.frame(response=test$Chla,fitted_values=pred.mlr,
+                         time=test$time)
+  Chla2.RMSE.mlr <- c(Chla2.RMSE.mlr,
+                      sqrt(sum((data.mlr$response-data.mlr$fitted_values)^2)/
+                             length(data.mlr$response)))
+  print(c('MLR',i))
+  # Generalized Linear Model
+  m <- glm(Chla~pH+DO+BOD+COD+SS+TN+TP+TOC+WT+EC+log(FC)+
+             NH3N+PO4P+log(TC)+Flow+Rain,data=train,
+           family=Gamma(link="log"))
+  m.step <- stepAIC(m, direction="both", trace=FALSE)
+  pred.glm <- predict(m.step,newdata=test,type="response")
+  data.glm <- data.frame(response=test$Chla,fitted_values=pred.glm,
+                         time=test$time)
+  Chla2.RMSE.glm <- c(Chla2.RMSE.glm,
+                      sqrt(sum((data.glm$response-data.glm$fitted_values)^2)/
+                             length(data.glm$response)))
+  print(c('GLM',i))
+  # Generalized Additive Model
+  mm.shrink <- gam(Chla~s(pH)+s(DO)+s(BOD)+s(COD)+s(SS)+s(TN)+s(TP)
+                   +s(TOC)+s(WT)+s(EC)+s(log(FC))+s(NH3N)+s(PO4P)
+                   +s(log(TC))+s(Flow)+s(Rain),data=train,
+                   family=quasi(link="log"),method="GCV.Cp",
+                   select=TRUE)
+  pred.gam <- predict(mm.shrink,newdata=test,type="response")
+  data.gam <- data.frame(response=test$Chla,fitted_values=pred.gam,
+                         time=test$time)
+  Chla2.RMSE.gam <- c(Chla2.RMSE.gam,
+                      sqrt(sum((data.gam$response-data.gam$fitted_values)^2)/
+                             length(data.gam$response)))
+  print(c('GAM',i))
+  # Time Varying Coefficient Model
+  vc.shrink <- gam(Chla~s(time)+s(time,by=pH)+s(time,by=DO)+
+                     s(time,by=BOD)+s(time,by=COD)+s(time,by=SS)+
+                     s(time,by=TN)+s(time,by=TP)+s(time,by=TOC)+
+                     s(time,by=WT)+s(time,by=EC)+s(time,by=log(FC))+
+                     s(time,by=NH3N)+s(time,by=PO4P)+s(time,by=log(TC))+
+                     s(time,by=Flow)+s(time,by=Rain),data=train,
+                   family=quasi(link="log"),method="GCV.Cp",
+                   select=TRUE)
+  pred.tvcm <- predict(vc.shrink,newdata=test,type="response")
+  data.tvcm <- data.frame(response=test$Chla,fitted_values=pred.tvcm,
+                          time=test$time)
+  Chla2.RMSE.tvcm <- c(Chla2.RMSE.tvcm,
+                       sqrt(sum((data.tvcm$response-data.tvcm$fitted_values)^2)/
+                              length(data.tvcm$response)))
+  print(c('TVCM',i))
+}
+Chla2.RMSE <- data.frame(RMSE=c(Chla2.RMSE.mlr,Chla2.RMSE.glm,
+                                Chla2.RMSE.gam,Chla2.RMSE.tvcm),
+                         model=c(rep("1_MLR",10),rep("2_GLM",10),
+                                 rep("3_GAM",10),rep("4_TVCM",10)))
+ggplot(Chla2.RMSE, aes(x=model, y=RMSE, fill=model)) + geom_boxplot() + 
+  ggtitle("Uchi Chla")
+
--- 논문데이터분석(GAM,VCmodel)/연습용csv파일/try5/그래프 그릴 경우 필요한 내용.R
+++ 논문데이터분석(GAM,VCmodel)/연습용csv파일/try5/그래프 그릴 경우 필요한 내용.R
@@ -3,36 +3,51 @@
 #### response vs fitted_values (train data (2010-2019))
 
 ```{r}
-pred.mlr <- predict(fit.step,newdata=ex1,type="response")
-data.mlr <- data.frame(response=ex1$TC,fitted_values=pred.mlr,time=ex1$time)
-sqrt(sum((data.mlr$response-data.mlr$fitted_values)^2)/length(data.mlr$response)) # RMSE = 28850.56   
+pred.mlr <- predict(gfit.step,newdata=ex2,type="response")
+data.mlr <- data.frame(response=ex2$TC,fitted_values=pred.mlr,
+                       time=ex2$time)
+sqrt(sum((data.mlr$response-data.mlr$fitted_values)^2)/
+       length(data.mlr$response)) # RMSE =   
 ggplot(data.mlr, aes(x=response, y=fitted_values)) +
   geom_point() + geom_rug() +
-  ggtitle("[Gwangsan TC train data] response vs fitted_values (Multiple Linear Regression)") +
+  ggtitle("[Uchi TC train data] response vs fitted_values 
+          (Multiple Linear Regression)") +
   geom_abline(intercept=0,slope=1,color='blue',size=1.5)
 par(mfrow=c(1,1))
-plot(data.mlr$time,data.mlr$response,type='o',col='blue',xlab="time",ylab="TC",main="[Gwangsan TC train data] response vs fitted_values (Multiple Linear Regression)",ylim=c(-100000,500000))
+plot(data.mlr$time,data.mlr$response,type='o',col='blue',
+     xlab="time",ylab="TC",main="[Uchi TC train data] 
+     response vs fitted_values (Multiple Linear Regression)",
+     ylim=c(-100000,500000))
 lines(data.mlr$time,data.mlr$fitted_values,type='o',col='red')
-legend(0,500000,c("fitted_values","response"),lwd=c(1,1),col=c("red","blue"))
+legend(0,500000,c("fitted_values","response"),lwd=c(1,1),
+       col=c("red","blue"))
 ```
 
 #### response vs fitted_values (test data (2020))
 
 ```{r}
-ex1_1 <- read.csv("C:/Users/HSY/Desktop/광산(2020).csv",sep=",",header=T)
-ex1_1 <- ex1_1[,-1]
-ex1_1 <- as.data.frame(ex1_1)
-pred.mlr1 <- predict(fit.step,newdata=ex1_1,type="response")
-data.mlr1 <- data.frame(response=ex1_1$TC,fitted_values=pred.mlr1,time=ex1_1$time)
-sqrt(sum((data.mlr1$response-data.mlr1$fitted_values)^2)/length(data.mlr1$response)) # RMSE = 18872.23   
+ex2_1 <- read.csv("C:/Users/HSY/Desktop/우치(2020).csv",
+                  sep=",",header=T)
+ex2_1 <- ex2_1[,-1]
+ex2_1 <- as.data.frame(ex2_1)
+pred.mlr1 <- predict(gfit.step,newdata=ex2_1,type="response")
+data.mlr1 <- data.frame(response=ex2_1$TC,fitted_values=pred.mlr1,
+                        time=ex2_1$time)
+sqrt(sum((data.mlr1$response-data.mlr1$fitted_values)^2)/
+       length(data.mlr1$response)) # RMSE =    
 ggplot(data.mlr1, aes(x=response, y=fitted_values)) +
   geom_point() + geom_rug() +
-  ggtitle("[Gwangsan TC test data] response vs fitted_values (Multiple Linear Regression)") +
+  ggtitle("[Uchi TC test data] response vs fitted_values 
+          (Multiple Linear Regression)") +
   geom_abline(intercept=0,slope=1,color='blue',size=1.5)
 par(mfrow=c(1,1))
-plot(data.mlr1$time,data.mlr1$response,type='o',col='blue',xlab="time",ylab="TC",main="[Gwangsan TC test data] response vs fitted_values (Multiple Linear Regression)",ylim=c(-100000,500000))
+plot(data.mlr1$time,data.mlr1$response,type='o',col='blue',
+     xlab="time",ylab="TC",main="[Uchi TC test data] 
+     response vs fitted_values (Multiple Linear Regression)",
+     ylim=c(-100000,500000))
 lines(data.mlr1$time,data.mlr1$fitted_values,type='o',col='red')
-legend(3650,500000,c("fitted_values","response"),lwd=c(1,1),col=c("red","blue"))
+legend(3650,500000,c("fitted_values","response"),lwd=c(1,1),
+       col=c("red","blue"))
 ```
 
 
@@ -42,36 +57,51 @@ legend(3650,500000,c("fitted_values","response"),lwd=c(1,1),col=c("red","blue"))
 #### response vs fitted_values (train data (2010-2019))
 
 ```{r}
-pred.glm <- predict(m.step,newdata=ex1,type="response")
-data.glm <- data.frame(response=ex1$TC,fitted_values=pred.glm,time=ex1$time)
-sqrt(sum((data.glm$response-data.glm$fitted_values)^2)/length(data.glm$response)) # RMSE = 21249.7    
+pred.glm <- predict(m.step,newdata=ex2,type="response")
+data.glm <- data.frame(response=ex2$TC,fitted_values=pred.glm,
+                       time=ex2$time)
+sqrt(sum((data.glm$response-data.glm$fitted_values)^2)/
+       length(data.glm$response)) # RMSE =    
 ggplot(data.glm, aes(x=response, y=fitted_values)) +
   geom_point() + geom_rug() +
-  ggtitle("[Gwangsan TC train data] response vs fitted_values (Generalized Linear Model)") +
+  ggtitle("[Uchi TC train data] response vs fitted_values 
+          (Generalized Linear Model)") +
   geom_abline(intercept=0,slope=1,color='blue',size=1.5)
 par(mfrow=c(1,1))
-plot(data.glm$time,data.glm$response,type='o',col='blue',xlab="time",ylab="TC",main="[Gwangsan TC train data] response vs fitted_values (Generalized Linear Model)",ylim=c(-100000,500000))
+plot(data.glm$time,data.glm$response,type='o',col='blue',
+     xlab="time",ylab="TC",main="[Uchi TC train data] 
+     response vs fitted_values (Generalized Linear Model)",
+     ylim=c(-100000,500000))
 lines(data.glm$time,data.glm$fitted_values,type='o',col='red')
-legend(0,500000,c("fitted_values","response"),lwd=c(1,1),col=c("red","blue"))
+legend(0,500000,c("fitted_values","response"),lwd=c(1,1),
+       col=c("red","blue"))
 ```
 
 #### response vs fitted_values (test data (2020))
 
 ```{r}
-ex1_1 <- read.csv("C:/Users/HSY/Desktop/광산(2020).csv",sep=",",header=T)
-ex1_1 <- ex1_1[,-1]
-ex1_1 <- as.data.frame(ex1_1)
-pred.glm1 <- predict(m.step,newdata=ex1_1,type="response")
-data.glm1 <- data.frame(response=ex1_1$TC,fitted_values=pred.glm1,time=ex1_1$time)
-sqrt(sum((data.glm1$response-data.glm1$fitted_values)^2)/length(data.glm1$response)) # RMSE = 28977.33  
+ex2_1 <- read.csv("C:/Users/HSY/Desktop/우치(2020).csv",
+                  sep=",",header=T)
+ex2_1 <- ex2_1[,-1]
+ex2_1 <- as.data.frame(ex2_1)
+pred.glm1 <- predict(m.step,newdata=ex2_1,type="response")
+data.glm1 <- data.frame(response=ex2_1$TC,fitted_values=pred.glm1,
+                        time=ex2_1$time)
+sqrt(sum((data.glm1$response-data.glm1$fitted_values)^2)/
+       length(data.glm1$response)) # RMSE =  
 ggplot(data.glm1, aes(x=response, y=fitted_values)) +
   geom_point() + geom_rug() +
-  ggtitle("[Gwangsan TC test data] response vs fitted_values (Generalized Linear Model)") +
+  ggtitle("[Uchi TC test data] response vs fitted_values 
+          (Generalized Linear Model)") +
   geom_abline(intercept=0,slope=1,color='blue',size=1.5)
 par(mfrow=c(1,1))
-plot(data.glm1$time,data.glm1$fitted_values,type='o',col='red',xlab="time",ylab="TC",main="[Gwangsan TC test data] response vs fitted_values (Generalized Linear Model)",ylim=c(-100000,500000))
+plot(data.glm1$time,data.glm1$fitted_values,type='o',col='red',
+     xlab="time",ylab="TC",main="[Uchi TC test data] 
+     response vs fitted_values (Generalized Linear Model)",
+     ylim=c(-100000,500000))
 lines(data.glm1$time,data.glm1$response,type='o',col='blue')
-legend(3650,500000,c("fitted_values","response"),lwd=c(1,1),col=c("red","blue"))
+legend(3650,500000,c("fitted_values","response"),lwd=c(1,1),
+       col=c("red","blue"))
 ```
 
 
@@ -81,36 +111,51 @@ legend(3650,500000,c("fitted_values","response"),lwd=c(1,1),col=c("red","blue"))
 #### response vs fitted_values (train data (2010-2019))
 
 ```{r}
-pred.gam <- predict(mm.shrink,newdata=ex1,type="response")
-data.gam <- data.frame(response=ex1$TC,fitted_values=pred.gam,time=ex1$time)
-sqrt(sum((data.gam$response-data.gam$fitted_values)^2)/length(data.gam$response)) # RMSE =     
+pred.gam <- predict(mm.shrink,newdata=ex2,type="response")
+data.gam <- data.frame(response=ex2$TC,fitted_values=pred.gam,
+                       time=ex2$time)
+sqrt(sum((data.gam$response-data.gam$fitted_values)^2)/
+       length(data.gam$response)) # RMSE =     
 ggplot(data.gam, aes(x=response, y=fitted_values)) +
   geom_point() + geom_rug() +
-  ggtitle("[Gwangsan TC train data] response vs fitted_values (Generalized Additive Model)") +
+  ggtitle("[Uchi TC train data] response vs fitted_values 
+          (Generalized Additive Model)") +
   geom_abline(intercept=0,slope=1,color='blue',size=1.5)
 par(mfrow=c(1,1))
-plot(data.gam$time,data.gam$response,type='o',col='blue',xlab="time",ylab="TC",main="[Gwangsan TC train data] response vs fitted_values (Generalized Additive Model)",ylim=c(-100000,500000))
+plot(data.gam$time,data.gam$response,type='o',col='blue',
+     xlab="time",ylab="TC",main="[Uchi TC train data] 
+     response vs fitted_values (Generalized Additive Model)",
+     ylim=c(-100000,500000))
 lines(data.gam$time,data.gam$fitted_values,type='o',col='red')
-legend(0,500000,c("fitted_values","response"),lwd=c(1,1),col=c("red","blue"))
+legend(0,500000,c("fitted_values","response"),lwd=c(1,1),
+       col=c("red","blue"))
 ```
 
 #### response vs fitted_values (test data (2020))
 
 ```{r}
-ex1_1 <- read.csv("C:/Users/HSY/Desktop/광산(2020).csv",sep=",",header=T)
-ex1_1 <- ex1_1[,-1]
-ex1_1 <- as.data.frame(ex1_1)
-pred.gam1 <- predict(mm.shrink,newdata=ex1_1,type="response")
-data.gam1 <- data.frame(response=ex1_1$TC,fitted_values=pred.gam1,time=ex1_1$time)
-sqrt(sum((data.gam1$response-data.gam1$fitted_values)^2)/length(data.gam1$response)) # RMSE =   
+ex2_1 <- read.csv("C:/Users/HSY/Desktop/우치(2020).csv",
+                  sep=",",header=T)
+ex2_1 <- ex2_1[,-1]
+ex2_1 <- as.data.frame(ex2_1)
+pred.gam1 <- predict(mm.shrink,newdata=ex2_1,type="response")
+data.gam1 <- data.frame(response=ex2_1$TC,fitted_values=pred.gam1,
+                        time=ex2_1$time)
+sqrt(sum((data.gam1$response-data.gam1$fitted_values)^2)/
+       length(data.gam1$response)) # RMSE =   
 ggplot(data.gam1, aes(x=response, y=fitted_values)) +
   geom_point() + geom_rug() +
-  ggtitle("[Gwangsan TC test data] response vs fitted_values (Generalized Additive Model)") +
+  ggtitle("[Uchi TC test data] response vs fitted_values 
+          (Generalized Additive Model)") +
   geom_abline(intercept=0,slope=1,color='blue',size=1.5)
 par(mfrow=c(1,1))
-plot(data.gam1$time,data.gam1$fitted_values,type='o',col='red',xlab="time",ylab="TC",main="[Gwangsan TC test data] response vs fitted_values (Generalized Additive Model)",ylim=c(-100000,500000))
+plot(data.gam1$time,data.gam1$fitted_values,type='o',col='red',
+     xlab="time",ylab="TC",main="[Uchi TC test data] 
+     response vs fitted_values (Generalized Additive Model)",
+     ylim=c(-100000,500000))
 lines(data.gam1$time,data.gam1$response,type='o',col='blue')
-legend(3650,500000,c("fitted_values","response"),lwd=c(1,1),col=c("red","blue"))
+legend(3650,500000,c("fitted_values","response"),lwd=c(1,1),
+       col=c("red","blue"))
 ```
 
 
@@ -120,34 +165,49 @@ legend(3650,500000,c("fitted_values","response"),lwd=c(1,1),col=c("red","blue"))
 #### response vs fitted_values (train data (2010-2019))
 
 ```{r}
-pred.tvcm <- predict(vc.shrink,newdata=ex1,type="response")
-data.tvcm <- data.frame(response=ex1$TC,fitted_values=pred.tvcm,time=ex1$time)
-sqrt(sum((data.tvcm$response-data.tvcm$fitted_values)^2)/length(data.tvcm$response)) # RMSE =     
+pred.tvcm <- predict(vc.shrink,newdata=ex2,type="response")
+data.tvcm <- data.frame(response=ex2$TC,fitted_values=pred.tvcm,
+                        time=ex2$time)
+sqrt(sum((data.tvcm$response-data.tvcm$fitted_values)^2)/
+       length(data.tvcm$response)) # RMSE =     
 ggplot(data.tvcm, aes(x=response, y=fitted_values)) +
   geom_point() + geom_rug() +
-  ggtitle("[Gwangsan TC train data] response vs fitted_values (Time Varying Coefficient Model)") +
+  ggtitle("[Uchi TC train data] response vs fitted_values 
+          (Time Varying Coefficient Model)") +
   geom_abline(intercept=0,slope=1,color='blue',size=1.5)
 par(mfrow=c(1,1))
-plot(data.tvcm$time,data.tvcm$response,type='o',col='blue',xlab="time",ylab="TC",main="[Gwangsan TC train data] response vs fitted_values (Time Varying Coefficient Model)",ylim=c(-100000,500000))
+plot(data.tvcm$time,data.tvcm$response,type='o',col='blue',
+     xlab="time",ylab="TC",main="[Uchi TC train data] 
+     response vs fitted_values (Time Varying Coefficient Model)",
+     ylim=c(-100000,500000))
 lines(data.tvcm$time,data.tvcm$fitted_values,type='o',col='red')
-legend(0,500000,c("fitted_values","response"),lwd=c(1,1),col=c("red","blue"))
+legend(0,500000,c("fitted_values","response"),lwd=c(1,1),
+       col=c("red","blue"))
 ```
 
 #### response vs fitted_values (test data (2020))
 
 ```{r}
-ex1_1 <- read.csv("C:/Users/HSY/Desktop/광산(2020).csv",sep=",",header=T)
-ex1_1 <- ex1_1[,-1]
-ex1_1 <- as.data.frame(ex1_1)
-pred.tvcm1 <- predict(vc.shrink,newdata=ex1_1,type="response")
-data.tvcm1 <- data.frame(response=ex1_1$TC,fitted_values=pred.tvcm1,time=ex1_1$time)
-sqrt(sum((data.tvcm1$response-data.tvcm1$fitted_values)^2)/length(data.tvcm1$response)) # RMSE =   
+ex2_1 <- read.csv("C:/Users/HSY/Desktop/우치(2020).csv",
+                  sep=",",header=T)
+ex2_1 <- ex2_1[,-1]
+ex2_1 <- as.data.frame(ex2_1)
+pred.tvcm1 <- predict(vc.shrink,newdata=ex2_1,type="response")
+data.tvcm1 <- data.frame(response=ex2_1$TC,fitted_values=pred.tvcm1,
+                         time=ex2_1$time)
+sqrt(sum((data.tvcm1$response-data.tvcm1$fitted_values)^2)/
+       length(data.tvcm1$response)) # RMSE =   
 ggplot(data.tvcm1, aes(x=response, y=fitted_values)) +
   geom_point() + geom_rug() +
-  ggtitle("[Gwangsan TC test data] response vs fitted_values (Time Varying Coefficient Model)") +
+  ggtitle("[Uchi TC test data] response vs fitted_values 
+          (Time Varying Coefficient Model)") +
   geom_abline(intercept=0,slope=1,color='blue',size=1.5)
 par(mfrow=c(1,1))
-plot(data.tvcm1$time,data.tvcm1$fitted_values,type='o',col='red',xlab="time",ylab="TC",main="[Gwangsan TC test data] response vs fitted_values (Time Varying Coefficient Model)",ylim=c(-100000,500000))
+plot(data.tvcm1$time,data.tvcm1$fitted_values,type='o',col='red',
+     xlab="time",ylab="TC",main="[Uchi TC test data] 
+     response vs fitted_values (Time Varying Coefficient Model)",
+     ylim=c(-100000,500000))
 lines(data.tvcm1$time,data.tvcm1$response,type='o',col='blue')
-legend(3650,500000,c("fitted_values","response"),lwd=c(1,1),col=c("red","blue"))
+legend(3650,500000,c("fitted_values","response"),lwd=c(1,1),
+       col=c("red","blue"))
 ```
