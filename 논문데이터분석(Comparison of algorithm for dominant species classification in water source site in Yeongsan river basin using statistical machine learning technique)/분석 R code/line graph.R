a866856064aa37d8508ead537af539be7d7d1c05
hsyliark <sungyoonhwang3@gmail.com>
2023-01-17 22:54:03 +0900

    add

---
+++ 논문데이터분석(Comparison of algorithm for dominant species classification in water source site in Yeongsan river basin using statistical machine learning technique)/분석 R code/line graph.R
@@ -0,0 +1,27 @@
+CJ1 <- read.csv("C:/Users/User/Desktop/논문데이터/J1 cell.csv",sep=",",header=T)
+CJ2 <- read.csv("C:/Users/User/Desktop/논문데이터/J2 cell.csv",sep=",",header=T)
+CT1 <- read.csv("C:/Users/User/Desktop/논문데이터/T1 cell.csv",sep=",",header=T)
+CT2 <- read.csv("C:/Users/User/Desktop/논문데이터/T2 cell.csv",sep=",",header=T)
+
+library(dplyr)
+CT2_reshape <- CT2 %>%
+  group_by(yyyymm) %>%
+  summarise_at(vars(c(blue,diatom,green,others)), list(name = mean))
+colnames(CT2_reshape) <- c("month","blue","diatom","green","others")
+
+library(reshape2)
+CT2_melt <- melt(CT2_reshape,
+                 id.vars = 'month',
+                 variable.name = "algae",
+                 value.name = "cells") 
+CT2_melt$month <- as.factor(CT2_melt$month)
+
+library(ggplot2)
+ggplot(CT2_melt, aes(x=month, y=cells, group=algae, color=algae)) +
+  geom_line(size=1.5) +
+  theme(axis.text.x=element_text(angle=90, hjust=1)) +
+  labs(x ="month", y = "mean of cells")
+
+
+
+
