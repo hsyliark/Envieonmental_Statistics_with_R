721b532e86e1bcbb4b3ae828f625be4c8d47a70a
hsyliark <sungyoonhwang3@gmail.com>
2024-02-03 12:20:22 +0900

    add

--- 논문데이터분석(2024 환기조)/R code/Correlation, SOM, PCA, Cluster_HSY_240204.R
+++ 논문데이터분석(2024 환기조)/R code/Correlation, SOM, PCA, Cluster_HSY_240204.R
@@ -1,7 +1,7 @@
 ### For data by day
 
 ## Loading data
-water_day <- read.csv("C:/Users/Hi/Desktop/2024 ?™˜ê²½ê¸°ì´ˆì¡°?‚¬?‚¬?—…/?°?´?„° ?ˆ˜ì§?/231219/?¼?ë£?/2013-2023 day.csv",             
+water_day <- read.csv("C:/Users/Hi/Desktop/2024 í™˜ê²½ê¸°ì´ˆì¡°ì‚¬ì‚¬ì—…/ë°ì´í„° ìˆ˜ì§‘/231214/ì¼ìë£Œ/2012-2023 ì¼ìë£Œ.csv",             
                       sep=",", header=T, fileEncoding = "CP949", encoding = "UTF-8")
 
 ## Correlation analysis
@@ -52,10 +52,11 @@ par(mfrow=c(1,1))
 ## PCA
 
 # by year average
-water <- read.csv("C:/Users/Hi/Desktop/2024 ?™˜ê²½ê¸°ì´ˆì¡°?‚¬?‚¬?—…/?°?´?„° ?ˆ˜ì§?/240102/st1.csv",
+water <- read.csv("C:/Users/Hi/Desktop/2024 í™˜ê²½ê¸°ì´ˆì¡°ì‚¬ì‚¬ì—…/ë°ì´í„° ìˆ˜ì§‘/240204/average 2019-2023.csv",
                   sep=",", header=T, fileEncoding = "CP949", encoding = "UTF-8")
 water_name <- water[,1]
 water <- water[,-1]
+water_scale <- data.frame(scale(water))
 rownames(water) <- water_name
 
 # Descriptive statistics
@@ -92,30 +93,43 @@ biplot(PCA_rot)
 # reference2 : https://www.statmethods.net/advstats/cluster.html
 # reference3 : https://rfriend.tistory.com/585
 
-# Distance matrix
-water_scale <- scale(water)
-d <- dist(water_scale, method="euclidean")
-as.matrix(d)[1:5,1:5]
-
 # Decide number of clusters 1 (NbClust)
 library(NbClust)
 nc <- NbClust(water_scale, distance="euclidean", method="ward.D",
-              min.nc=1, max.nc=14)
+              min.nc=1, max.nc=13)
 par(mfrow=c(1,1))
 plot(fit)
 rect.hclust(fit, k=3)
 
 # Decide number of clusters 2 (fviz_nbclust)
-fviz_nbclust(water_scale, FUNcluster = clara,
-  method = c("silhouette"), diss = NULL,
-  k.max = 10, nboot = 100, verbose = interactive(),
-  barfill = "steelblue",
-  barcolor = "steelblue",
-  linecolor = "steelblue",
-  print.summary = TRUE)
+library(factoextra)
+fviz_nbclust(water_scale, 
+             FUNcluster = kmeans,
+             method = c("silhouette"), diss = NULL,
+             k.max = 10, nboot = 100, verbose = interactive(),
+             barfill = "steelblue",
+             barcolor = "steelblue",
+             linecolor = "steelblue",
+             print.summary = TRUE)
+
+# Distance matrix
+water_scale <- scale(water)
+d <- dist(water_scale, method="euclidean")
+as.matrix(d)[1:5,1:5]
 
 # Apply Distance matrix model
 ## Hierarchical Cluster Analysis
+# Decide number of clusters
+library(factoextra)
+fviz_nbclust(water_scale, 
+             FUNcluster = hcut,
+             method = c("silhouette"), diss = NULL,
+             k.max = 10, nboot = 100, verbose = interactive(),
+             barfill = "steelblue",
+             barcolor = "steelblue",
+             linecolor = "steelblue",
+             print.summary = TRUE)
+d <- dist(water_scale, method="euclidean")
 fit <- hclust(d, method="ward.D")
 plot(fit)
 rect.hclust(fit, k=4)
@@ -123,19 +137,50 @@ rect.hclust(fit, k=4)
 ## fviz_silhouette: Visualize Silhouette Information from Clustering
 library(factoextra)
 ## K-means clustering
-set.seed(1004)
+# Decide number of clusters
+fviz_nbclust(water_scale, 
+             FUNcluster = kmeans,
+             method = c("silhouette"), diss = NULL,
+             k.max = 10, nboot = 100, verbose = interactive(),
+             barfill = "steelblue",
+             barcolor = "steelblue",
+             linecolor = "steelblue",
+             print.summary = TRUE)
 km.res <- kmeans(water_scale, centers=4)
 km.res[["cluster"]]
 # Visualize silhouhette information
 library(cluster)
 sil <- silhouette(km.res$cluster, dist(water_scale, method="euclidean"))
 fviz_silhouette(sil)
+# visualizing K-means clusters
+fviz_cluster(km.res, data = water_scale,
+             choose.vars = NULL, stand = TRUE,
+             axes = c(1, 2), geom = c("point", "text"),
+             repel = FALSE, show.clust.cent = TRUE,
+             ellipse = TRUE, ellipse.type = "convex",
+             ellipse.level = 0.95, ellipse.alpha = 0.2,
+             shape = NULL, pointsize = 1.5,
+             labelsize = 12, main = "Cluster plot",
+             xlab = NULL, ylab = NULL,
+             outlier.color = "black", outlier.shape = 19,
+             outlier.pointsize = pointsize, outlier.labelsize = labelsize,
+             ggtheme = theme_grey()
+)
 
 ## Partitioning Around Medoids
 # reference : https://www.datanovia.com/en/lessons/k-medoids-in-r-algorithm-and-practical-examples/
 library(cluster)
 library(factoextra)
-pam.res <- pam(water_scale, k=3)
+# Decide number of clusters
+fviz_nbclust(water_scale, 
+             FUNcluster = pam,
+             method = c("silhouette"), diss = NULL,
+             k.max = 10, nboot = 100, verbose = interactive(),
+             barfill = "steelblue",
+             barcolor = "steelblue",
+             linecolor = "steelblue",
+             print.summary = TRUE)
+pam.res <- pam(water_scale, k=5)
 print(pam.res)
 # visualizing PAM clusters
 fviz_cluster(pam.res, data = water_scale,
@@ -153,14 +198,26 @@ fviz_cluster(pam.res, data = water_scale,
   )
 
 ## Gaussian Mixture Model
-# reference : https://search.r-project.org/CRAN/refmans/ClusterR/html/GMM.html
-
+# reference 1 : https://search.r-project.org/CRAN/refmans/ClusterR/html/GMM.html
+# reference 2 : https://cran.r-project.org/web/packages/ClusterR/vignettes/the_clusterR_package.html
 # Install packages
 library(ClusterR)
 dat <- as.matrix(water_scale)
 dat <- center_scale(dat)
-gmm <- GMM(dat, 4, dist_mode="maha_dist", seed_mode="random_subset", 
+# Decide number of clusters
+opt_gmm <- Optimal_Clusters_GMM(
+  dat, max_clusters = 10, criterion = "AIC", 
+  dist_mode = "maha_dist", seed_mode = "random_subset",
+  km_iter = 10, em_iter = 10, var_floor = 1e-10, 
+  plot_data = T)
+opt_gmm <- Optimal_Clusters_GMM(
+  dat, max_clusters = 10, criterion = "BIC", 
+  dist_mode = "maha_dist", seed_mode = "random_subset",
+  km_iter = 10, em_iter = 10, var_floor = 1e-10, 
+  plot_data = T)
+gmm <- GMM(dat, 5, dist_mode="maha_dist", seed_mode="random_subset", 
            km_iter=10, em_iter=10)
+gmm[["Log_likelihood"]]
 
 ## SOM cluster
 # reference : https://woosa7.github.io/R-Clustering-Kmens-SOM/
@@ -184,15 +241,12 @@ str(som_model2)
 
 # Visualization
 plot(som_model1, main="feature distribution")
+som_model1[["unit.classif"]]
 table(som_model2$clustering)
 plot(som_model2, what="prototypes", type="umatrix", print.title=T)
 plot(som_model2, what="obs", type="names", print.title=T, scale=c(1,1))
+som_model2[["clustering"]]
 plot(som_model1, type="counts", main="cluster size")
-
-par(mfrow=c(1,2))
-plot(som_model1, main="feature distribution")
-plot(som_model2, what="obs", type="names", print.title=T, scale=c(1,1))
-par(mfrow=c(1,1))
 
 # Clustering results
 clusters <- superClass(model, k=5)
@@ -201,70 +255,66 @@ plot(clusters)
 plot(clusters, type="dendro3d")
 
 ## Density plot with group
-st3 <- read.csv("C:/Users/Hi/Desktop/2024 ?™˜ê²½ê¸°ì´ˆì¡°?‚¬?‚¬?—…/?°?´?„° ?ˆ˜ì§?/240102/st3 group.csv",
-                  sep=",", header=T, fileEncoding = "CP949", encoding = "UTF-8")
-st3_name <- st3[,1]
-st3 <- st3[,-1]
-rownames(st3) <- st3_name
-st3$K_means <- as.factor(st3$K_means)
-st3$GMM <- as.factor(st3$GMM)
-st3$SOM <- as.factor(st3$SOM)
+water$cluster <- c(5,2,3,3,2,1,4,1,4,1,5,2,4)
+water$cluster <- as.factor(water$cluster)
 
 library(ggplot2)
-ggplot(st3, aes(x=NH3N, fill=SOM)) +
+# BOD, COD, SS, TN, NO3N, NH3N, TP, PO4P, Chla, TOC
+# animal, manure, land
+ggplot(water, aes(x=land, fill=cluster)) +
   geom_density(alpha=0.4)
 
 
 ## PCA after clustering
-k1 <- read.csv("C:/Users/Hi/Desktop/2024 ?™˜ê²½ê¸°ì´ˆì¡°?‚¬?‚¬?—…/?°?´?„° ?ˆ˜ì§?/231227/CA/K-means/k1_2023.csv",  
+k1 <- read.csv("C:/Users/Hi/Desktop/2024 ???ê²½ê¸°ì´ˆì¡°?????????/????????? ??????/231227/CA/K-means/k1_2023.csv",  
                   sep=",", header=T, fileEncoding = "CP949", encoding = "UTF-8")
 k1_name <- k1[,1]
 k1 <- k1[,-1]
 rownames(k1) <- k1_name
 
-k2 <- read.csv("C:/Users/Hi/Desktop/2024 ?™˜ê²½ê¸°ì´ˆì¡°?‚¬?‚¬?—…/?°?´?„° ?ˆ˜ì§?/231227/CA/K-means/k2_2023.csv",  
+k2 <- read.csv("C:/Users/Hi/Desktop/2024 ???ê²½ê¸°ì´ˆì¡°?????????/????????? ??????/231227/CA/K-means/k2_2023.csv",  
                sep=",", header=T, fileEncoding = "CP949", encoding = "UTF-8")
 k2_name <- k2[,1]
 k2 <- k2[,-1]
 rownames(k2) <- k2_name
 
-k3 <- read.csv("C:/Users/Hi/Desktop/2024 ?™˜ê²½ê¸°ì´ˆì¡°?‚¬?‚¬?—…/?°?´?„° ?ˆ˜ì§?/231227/CA/K-means/k3_2023.csv",  
+k3 <- read.csv("C:/Users/Hi/Desktop/2024 ???ê²½ê¸°ì´ˆì¡°?????????/????????? ??????/231227/CA/K-means/k3_2023.csv",  
                sep=",", header=T, fileEncoding = "CP949", encoding = "UTF-8")
 k3_name <- k3[,1]
 k3 <- k3[,-1]
 rownames(k3) <- k3_name
 
-g1 <- read.csv("C:/Users/Hi/Desktop/2024 ?™˜ê²½ê¸°ì´ˆì¡°?‚¬?‚¬?—…/?°?´?„° ?ˆ˜ì§?/231227/CA/GMM/g1_2023.csv",  
+g1 <- read.csv("C:/Users/Hi/Desktop/2024 ???ê²½ê¸°ì´ˆì¡°?????????/????????? ??????/231227/CA/GMM/g1_2023.csv",  
                sep=",", header=T, fileEncoding = "CP949", encoding = "UTF-8")
 g1_name <- g1[,1]
 g1 <- g1[,-1]
 rownames(g1) <- g1_name
 
-g2 <- read.csv("C:/Users/Hi/Desktop/2024 ?™˜ê²½ê¸°ì´ˆì¡°?‚¬?‚¬?—…/?°?´?„° ?ˆ˜ì§?/231227/CA/GMM/g2_2023.csv",  
+g2 <- read.csv("C:/Users/Hi/Desktop/2024 ???ê²½ê¸°ì´ˆì¡°?????????/????????? ??????/231227/CA/GMM/g2_2023.csv",  
                sep=",", header=T, fileEncoding = "CP949", encoding = "UTF-8")
 g2_name <- g2[,1]
 g2 <- g2[,-1]
 rownames(g2) <- g2_name
 
-g3 <- read.csv("C:/Users/Hi/Desktop/2024 ?™˜ê²½ê¸°ì´ˆì¡°?‚¬?‚¬?—…/?°?´?„° ?ˆ˜ì§?/231227/CA/GMM/g3_2023.csv",  
+g3 <- read.csv("C:/Users/Hi/Desktop/2024 ???ê²½ê¸°ì´ˆì¡°?????????/????????? ??????/231227/CA/GMM/g3_2023.csv",  
                sep=",", header=T, fileEncoding = "CP949", encoding = "UTF-8")
 g3_name <- g3[,1]
 g3 <- g3[,-1]
 rownames(g3) <- g3_name
 
-s1 <- read.csv("C:/Users/Hi/Desktop/2024 ?™˜ê²½ê¸°ì´ˆì¡°?‚¬?‚¬?—…/?°?´?„° ?ˆ˜ì§?/231227/CA/SOM/s1_2023.csv",  
+s1 <- read.csv("C:/Users/Hi/Desktop/2024 ???ê²½ê¸°ì´ˆì¡°?????????/????????? ??????/231227/CA/SOM/s1_2023.csv",  
                sep=",", header=T, fileEncoding = "CP949", encoding = "UTF-8")
 s1_name <- s1[,1]
 s1 <- s1[,-1]
 rownames(s1) <- s1_name
 
-s2 <- read.csv("C:/Users/Hi/Desktop/2024 ?™˜ê²½ê¸°ì´ˆì¡°?‚¬?‚¬?—…/?°?´?„° ?ˆ˜ì§?/231227/CA/SOM/s2_2023.csv",  
+s2 <- read.csv("C:/Users/Hi/Desktop/2024 ???ê²½ê¸°ì´ˆì¡°?????????/????????? ??????/231227/CA/SOM/s2_2023.csv",  
                sep=",", header=T, fileEncoding = "CP949", encoding = "UTF-8")
 s2_name <- s2[,1]
 s2 <- s2[,-1]
 rownames(s2) <- s2_name
 
-s3 <- read.csv("C:/Users/Hi/Desktop/2024 ?™˜ê²½ê¸°ì´ˆì¡°?‚¬?‚¬?—…/?°?´?„° ?ˆ˜ì§?/231227/CA/SOM/s3_2023.csv",  
+s3 <- read.csv("C:/Users/Hi/Desktop/2024 ???ê²½ê¸°ì´ˆì¡°?????????/????????? ??????/231227/CA/SOM/s3_2023.csv",  
                sep=",", header=T, fileEncoding = "CP949", encoding = "UTF-8")
 s3_name <- s3[,1]
 s3 <- s3[,-1]
