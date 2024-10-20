d08c289ab7a7cf7cc3ee978f7e3cc3988ec30db2
hsyliark <sungyoonhwang3@gmail.com>
2024-10-21 00:48:17 +0900

    add

---
+++ 논문데이터분석(Chl-a predict, DNN and CNN with SMOTE)/논문_Chla_SMOTE_241020_HSY/모델 성능평가 지표.R
@@ -0,0 +1,14 @@
+res_SC <- read.csv("C:/Users/User/Desktop/df_SC_res.csv",
+                   sep=",",header=T)
+res_JS <- read.csv("C:/Users/User/Desktop/df_JS_res.csv",
+                   sep=",",header=T)
+
+RMSE <- function(y, yhat) {sqrt(sum((y-yhat)^2)/length(y))}
+MAE <- function(y, yhat) {sum(abs(y-yhat))/length(y)}
+MAPE <- function(y, yhat) {sum(abs((y-yhat)/y))*100/length(y)}
+NMB <- function(y, yhat) {100*sum((y-yhat)/y)}
+IOA <- function(y, yhat) {1-(sum((y-yhat)^2))/(sum((abs(yhat-mean(y))+abs(y-mean(y)))^2))} 
+
+IOA(res_SC$Chla, res_SC$CNN_SMOTE_Tomek)
+
+IOA(res_JS$Chla, res_JS$LSTM)
